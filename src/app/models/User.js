49dee0a7fcb0490bf1be734b15ddf2029df21c1a
Victor Vuelma <victorvuelma@outlook.com>
2019-11-20 17:29:48 -0300

    fix user model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -4,7 +4,6 @@ class User extends Model {
   static init(sequelize) {
     super.init(
       {
-        id: Sequelize.INTEGER,
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
