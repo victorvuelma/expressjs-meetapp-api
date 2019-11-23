97e793a3d2e84d4ca5e6bf12a3ff26008053af0c
Victor Vuelma <victorvuelma@outlook.com>
2019-11-23 16:21:37 -0300

    add templates and configure subscription send mail
    Signed-off-by: Victor Vuelma <victorvuelma@outlook.com>

--- src/app/job/SubscriptionOrganizerMail.js
+++ src/app/job/SubscriptionOrganizerMail.js
@@ -1,9 +1,26 @@
+import Mail from '../../lib/Mail';
+
 class SubscriptionOrganizerMail {
   get key() {
     return 'SubscriptionOrganizerMail';
   }
 
-  handle({ data }) {}
+  async handle({ data }) {
+    const { meetup, user } = data;
+    const { user: organizer } = meetup;
+
+    await Mail.sendMail({
+      to: `${organizer.name} <${organizer.email}>`,
+      subject: `Novo credenciamento em ${meetup.title}`,
+      text: 'Você tem um novo credenciamento',
+      template: 'subscription_organizer',
+      context: {
+        meetup: meetup.title,
+        user: user.name,
+        organizer: organizer.name,
+      },
+    });
+  }
 }
 
 export default new SubscriptionOrganizerMail();
