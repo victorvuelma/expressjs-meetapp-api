b5963e388346145616fc6e0cf166ae775cecda52
Victor Vuelma <victorvuelma@outlook.com>
2019-11-22 23:32:06 -0300

    fix models associations

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -15,11 +15,13 @@ class User extends Model {
       }
     );
 
-    super.addHook('beforeSave', async user => {
+    this.addHook('beforeSave', async user => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
     });
+
+    return this;
   }
 
   checkPassword(password) {
