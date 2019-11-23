f98318c0c4ad94fba877c0ca061a1d5564e3daa2
Victor Vuelma <victorvuelma@outlook.com>
2019-11-23 13:17:55 -0300

    add subscription model

---
+++ src/database/migrations/20191123160923-create-subscriptions.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('subscriptions', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      meetup_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'meetups',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('subscriptions');
+  },
+};
