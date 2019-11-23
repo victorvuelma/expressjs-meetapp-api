e9d3090ce575a4a0c7661aea112f7d234eb964d2
Victor Vuelma <victorvuelma@outlook.com>
2019-11-23 10:47:55 -0300

    add meetup model

---
+++ src/app/models/Meetup.js
@@ -0,0 +1,32 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, {
+      foreignKey: 'image_id',
+      as: 'image',
+    });
+    this.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'user',
+    });
+  }
+}
+
+export default Meetup;
---
+++ src/database/migrations/20191123133921-create-meetups.js
@@ -0,0 +1,60 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('meetups', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      location: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      image_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'files',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('meetups');
+  },
+};
